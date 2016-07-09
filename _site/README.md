dec32e857f8258be7b4fb3490f5b8715d2b91e44
Richard Littauer <richard.littauer@gmail.com>
2016-07-10 00:39:38 +0100

    Update website

---
+++ _site/README.md
@@ -0,0 +1,6 @@
+Octobuild
+=========
+
+Go to [http://octobuild.com](http://octobuild.com).
+
+All of the work pretty much goes on in index.md, and on the gh-pages branch.
---
+++ _site/javascripts/site.js
@@ -0,0 +1,28 @@
+
+/*
+ * parse_link_header()
+ *
+ * Parse the Github Link HTTP header used for pageination
+ * http://developer.github.com/v3/#pagination
+ */
+function parse_link_header(header) {
+  if (header.length == 0) {
+    throw new Error("input must not be of zero length");
+  }
+
+  // Split parts by comma
+  var parts = header.split(',');
+  var links = {};
+  // Parse each part into a named link
+  _.each(parts, function(p) {
+    var section = p.split(';');
+    if (section.length != 2) {
+      throw new Error("section could not be split on ';'");
+    }
+    var url = section[0].replace(/<(.*)>/, '$1').trim();
+    var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+    links[name] = url;
+  });
+
+  return links;
+}
